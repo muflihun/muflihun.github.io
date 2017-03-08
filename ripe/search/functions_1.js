e848b9e2d0e30a42f96b80c52261cc52ed724485
Majid Khan <mkhan3189@gmail.com>
2017-03-08 20:58:07 +1100

    2.2.0 doc

--- ripe/search/functions_1.js
+++ ripe/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['decryptaes',['decryptAES',['../class_ripe.html#aca374ac864754120146fa10d7809f602',1,'Ripe::decryptAES(const std::string &amp;data, const byte *key, std::size_t keySize, std::vector&lt; byte &gt; &amp;iv)'],['../class_ripe.html#a57907b7a33b7951c2253a1599896667a',1,'Ripe::decryptAES(std::string &amp;data, const std::string &amp;hexKey, std::string &amp;iv, bool isBase64=false, bool isHex=false)']]],
-  ['decryptrsa',['decryptRSA',['../class_ripe.html#ad953ef353a93d95774991c100d03288b',1,'Ripe::decryptRSA(const std::string &amp;data, const std::string &amp;privateKeyPEM, const std::string &amp;secret=&quot;&quot;)'],['../class_ripe.html#acd267e5444eb203b8d35a6cc4a8b8412',1,'Ripe::decryptRSA(std::string &amp;data, const std::string &amp;key, bool isBase64, int length=DEFAULT_RSA_LENGTH, const std::string &amp;secret=&quot;&quot;)']]]
+  ['decryptrsa',['decryptRSA',['../class_ripe.html#ad953ef353a93d95774991c100d03288b',1,'Ripe::decryptRSA(const std::string &amp;data, const std::string &amp;privateKeyPEM, const std::string &amp;secret=&quot;&quot;)'],['../class_ripe.html#afc2a834d7a596ccd9050a7b8067f81ce',1,'Ripe::decryptRSA(std::string &amp;data, const std::string &amp;key, bool isBase64, bool isHex=false, const std::string &amp;secret=&quot;&quot;)']]]
 ];
