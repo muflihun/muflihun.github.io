d1056365aefef2d57e3709a3e1f2edd96215ea63
mkhan <mkhan@allhomes.com.au>
2017-07-07 16:31:49 +1000

    Updated residue to 1.0.0-beta.4

--- residue/docs/search/all_3.js
+++ residue/docs/search/all_3.js
@@ -4,5 +4,6 @@ var searchData=
   ['enablebulkdispatch',['enableBulkDispatch',['../class_residue.html#a5521146d9286ff7488868e3579751130',1,'Residue']]],
   ['enablecrashhandler',['enableCrashHandler',['../class_residue.html#aa09447e36d17fe8476e6e6373a108527',1,'Residue']]],
   ['enableplainrequest',['enablePlainRequest',['../class_residue.html#acd477bb677ccc6bcb9aea2473f90b697',1,'Residue']]],
-  ['enableutc',['enableUtc',['../class_residue.html#a9327141f313dbc26636dff96802a8f13',1,'Residue']]]
+  ['enableutc',['enableUtc',['../class_residue.html#a9327141f313dbc26636dff96802a8f13',1,'Residue']]],
+  ['errors',['errors',['../class_residue.html#aadaafabd646e1965cb7d94a9a0a1cbb7',1,'Residue']]]
 ];
