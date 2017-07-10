4fcb12e91fcf5ca4b0b6aa3720c07d3088c1f2d6
mkhan <mkhan@allhomes.com.au>
2017-07-10 15:20:50 +1000

    update doc

--- ripe/docs/search/all_c.js
+++ ripe/docs/search/all_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]],
   ['ripe',['Ripe',['../class_ripe.html',1,'']]]
 ];
--- ripe/docs/search/functions_4.js
+++ ripe/docs/search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['generatenewkey',['generateNewKey',['../class_ripe.html#adf3c0c52df3c81807f631c0e27819a73',1,'Ripe']]],
-  ['generatersakeypair',['generateRSAKeyPair',['../class_ripe.html#a704a0f41f62e8aaa0e94607c1545ead9',1,'Ripe']]],
-  ['generatersakeypairbase64',['generateRSAKeyPairBase64',['../class_ripe.html#ad2625c9619dd50613458fedb672aa1ea',1,'Ripe']]]
+  ['generatersakeypair',['generateRSAKeyPair',['../class_ripe.html#a680305b6eb2eeb85b2cf9a81dcf32aa1',1,'Ripe']]],
+  ['generatersakeypairbase64',['generateRSAKeyPairBase64',['../class_ripe.html#a6d4b1ac38cee624cedbeaf599e5e3f31',1,'Ripe']]]
 ];
