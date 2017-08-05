e6cf8c522d391465c4ef5510e04bbcd53e88c900
Majid Khan <mkhan3189@gmail.com>
2017-08-06 01:43:14 +1000

    version docs

--- residue/docs/search/searchdata.js
+++ residue/docs/search/searchdata.js
@@ -1,30 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acdefghilmprsvw",
-  1: "r",
-  2: "cdeghilmrsvw",
-  3: "dlps",
-  4: "a",
-  5: "f"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "functions",
-  3: "variables",
-  4: "typedefs",
-  5: "enums"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Functions",
-  3: "Variables",
-  4: "Typedefs",
-  5: "Enumerations"
 };
 
