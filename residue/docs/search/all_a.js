45a491c9ad53e41ae4e9c4e42aa5cce629711277
mkhan <mkhan@allhomes.com.au>
2017-08-13 20:36:37 +1000

    updated docs

--- residue/docs/search/all_a.js
+++ residue/docs/search/all_a.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['ping_5fthreshold',['PING_THRESHOLD',['../class_residue.html#ab6c33b99af70312d46d02a5276200c79',1,'Residue']]]
+  ['reconnect',['reconnect',['../class_residue.html#a577711d27045b804cff65505fc506382',1,'Residue']]],
+  ['residue',['Residue',['../class_residue.html',1,'']]],
+  ['residueexception',['ResidueException',['../class_residue_exception.html',1,'ResidueException'],['../class_residue_exception.html#a19e34815839bc780d1bd8eddd97b5698',1,'ResidueException::ResidueException()']]]
 ];
