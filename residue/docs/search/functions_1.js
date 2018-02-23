11caff34f99185e8cb7cb0fcccb81358d4a99011
Majid <mkhan3189@gmail.com>
2018-02-23 18:32:38 +1100

    removed plain log

--- residue/docs/search/functions_1.js
+++ residue/docs/search/functions_1.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['disableautobulkparams',['disableAutoBulkParams',['../class_residue.html#a262f73f8f6c4d749f4a7fd4bccce8fbf',1,'Residue']]],
   ['disablebulkdispatch',['disableBulkDispatch',['../class_residue.html#affc470c515fabf8b9bcc7a63965a57dc',1,'Residue']]],
-  ['disableplainrequest',['disablePlainRequest',['../class_residue.html#abfea282f667ce19acc58d50922ce36be',1,'Residue']]],
   ['disableutc',['disableUtc',['../class_residue.html#acc949f1c8b0530d79b11f60c31bfe5b0',1,'Residue']]],
   ['disconnect',['disconnect',['../class_residue.html#ab1a642f4428bb02e82a52bcbcab9df3f',1,'Residue']]]
 ];
